6df1021361bdab53da944ef569301c6809e99ea2
duzhiguang <525874600@qq.com>
2020-12-01 10:12:49 +0800

    添加OSS依赖，测试OSS

--- src/main/java/com/example/demo/utils/OssUtil.java
+++ src/main/java/com/example/demo/utils/OssUtil.java
@@ -1,5 +1,11 @@
 package com.example.demo.utils;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+
+import java.io.ByteArrayInputStream;
+
 /**
  * ClassName：OssUtil
  * Package:com.example.demo.utils
@@ -11,7 +17,28 @@ package com.example.demo.utils;
 public class OssUtil {
 
 
+    private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
+    private static final String ACCESS_KEY_ID = "LTAI4GB32mSWG2NceyX3qs7g";
+    private static final String ACCESS_KEY_SECRET = "QYW1TlQT7Tg5nz448FvvosCSZE68RQ";
+    private static final String BUCKET_NAME = "edu-guli-20200918";
 
+    public static void createBucket(){
+        OSS client = new OSSClientBuilder().build(ENDPOINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+        client.createBucket(BUCKET_NAME);
+        client.shutdown();
+    }
+
+    public static void uploadFile(){
+        String objectName = "G:\\勤务通打卡用户手册20200112(1).pdf";
+        String content = "Hello OSS";
+        OSS client = new OSSClientBuilder().build(ENDPOINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+        client.putObject(BUCKET_NAME, objectName, new ByteArrayInputStream(content.getBytes()));
+        client.shutdown();
+    }
+
+    public static void main(String[] args) {
+        uploadFile();
+    }
 
 
 }
